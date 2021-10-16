b46d0032c99b52b0a377c52aa6487c1d100df971
diolan12 <dio_lantief21@outlook.com>
2021-10-17 06:58:05 +0700

    Update Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,5 +6,46 @@ use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    //
+    protected $c;
+    protected $b;
+
+    /**
+     * Returning success response.
+     * 
+     * @return JsonResponse
+     */
+    protected function success($content, int $code = 200)
+    {
+        $this->c = $code;
+        $this->b = [
+            'type' => 'SUCCESS',
+            'content' => $content
+        ];
+        return $this->response();
+    }
+
+    /**
+     * Returning error response.
+     * 
+     * @return JsonResponse
+     */
+    protected function error($content, int $code = 400)
+    {
+        $this->c = $code;
+        $this->b = [
+            'type' => 'ERROR',
+            'content' => $content
+        ];
+        return $this->response();
+    }
+
+    /**
+     * Returning response with predefined http code and body content.
+     *
+     * @return JsonResponse
+     */
+    protected function response()
+    {
+        return response()->json($this->b, $this->c);
+    }
 }
