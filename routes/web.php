e572b8f7d7f5c3d8cb02869ceade9e0f850c9c78
diolan12 <dio_lantief21@outlook.com>
2021-10-17 03:10:24 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -20,8 +20,10 @@ $router->group(['prefix' => 'auth'], function () use ($router) {
     $router->post('login', ['uses' => 'AuthController@login']);
     $router->get('verify', ['uses' => 'AuthController@verify']);
     // Todo GET 'account' return object account ::account()
+
     // Todo POST 'profile' return generic success/error ::uploadPicture()
     $router->post('photo', ['uses' => 'AuthController@uploadPicture']);
+    
     // Todo POST 'password' required[oldPassword, newPassword, verifyPassword] return generic success/error ::changePassword()
 
 });
