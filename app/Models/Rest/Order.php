85801f404fd6fb8453a1803607ac2690c3f2c0b9
diolan12 <dio_lantief21@outlook.com>
2021-11-20 08:54:32 +0700

    Update Order.php

--- app/Models/Rest/Order.php
+++ app/Models/Rest/Order.php
@@ -4,15 +4,33 @@ namespace App\Models\Rest;
 
 use App\Models\BaseModel;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Haruncpi\LaravelIdGenerator\IdGenerator;
+use Carbon\Carbon;
 
 class Order extends BaseModel
 {
     use SoftDeletes;
 
+    public static function boot(){
+        parent::boot();
+        self::creating(function ($model) {
+            // SC-2021 11 20 00001
+            $carbon = Carbon::now('Asia/Jakarta');
+            $month = (strlen($carbon->month) == 1) ? '0'.$carbon->month : $carbon->month;
+            $day = (strlen($carbon->day) == 1) ? '0'.$carbon->day : $carbon->day;
+            $model->uid = (string)IdGenerator::generate([
+                'table' => $this->table,
+                'length' => 5,
+                'prefix' => 'SC-'. $carbon->year . $month . $day,
+            ]);
+        });
+    }
+
     public function validation()
     {
         return [
-            'uid' => 'required|unique:' . $this->getTable(),
+            // 'uid' => 'required|unique:' . $this->getTable(),
+            'field' => '',
             'office' => 'required',
             'status' => 'required',
             'customer' => 'required',
@@ -31,7 +49,7 @@ class Order extends BaseModel
      * @var array
      */
     protected $fillable = [
-        'uid', 'office', 'field', 'status', 'customer', 'service_type'
+        'field', 'office', 'field', 'status', 'customer', 'service_type'
     ];
 
     /**
