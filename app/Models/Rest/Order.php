4aff4da3560fe5f536d2b2cbcd948bef1be6bd18
diolan12 <dio_lantief21@outlook.com>
2021-11-21 03:26:06 +0700

    Update Order.php

--- app/Models/Rest/Order.php
+++ app/Models/Rest/Order.php
@@ -10,21 +10,8 @@ use Carbon\Carbon;
 class Order extends BaseModel
 {
     use SoftDeletes;
+    protected $table = 'orders';
 
-    public static function boot(){
-        parent::boot();
-        self::creating(function ($model) {
-            // SC-2021 11 20 00001
-            $carbon = Carbon::now('Asia/Jakarta');
-            $month = (strlen($carbon->month) == 1) ? '0'.$carbon->month : $carbon->month;
-            $day = (strlen($carbon->day) == 1) ? '0'.$carbon->day : $carbon->day;
-            $model->uid = (string)IdGenerator::generate([
-                'table' => $this->table,
-                'length' => 5,
-                'prefix' => 'SC-'. $carbon->year . $month . $day,
-            ]);
-        });
-    }
 
     public function validation()
     {
@@ -34,12 +21,23 @@ class Order extends BaseModel
             'office' => 'required',
             'status' => 'required',
             'customer' => 'required',
-            'service_type' => 'required',
+            'service' => 'required',
         ];
     }
     public function filter($data)
     {
         unset($data['id']);
+
+        // SC-20212010001
+        $carbon = Carbon::now('Asia/Jakarta');
+        $month = (strlen($carbon->month) == 1) ? '0'.$carbon->month : $carbon->month;
+        $day = (strlen($carbon->day) == 1) ? '0'.$carbon->day : $carbon->day;
+        $data['uid'] = (string)IdGenerator::generate([
+            'table' => $this->table,
+            'field' => 'uid',
+            'length' => 14,
+            'prefix' => 'SC-'. $carbon->year . $month . $day,
+        ]);
         return $data;
     }
 
@@ -49,7 +47,7 @@ class Order extends BaseModel
      * @var array
      */
     protected $fillable = [
-        'field', 'office', 'field', 'status', 'customer', 'service_type'
+        'field', 'office', 'field', 'status', 'customer', 'service'
     ];
 
     /**
