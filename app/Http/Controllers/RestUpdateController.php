19cb36753894a4ee4650e359a4f145a0ea104ffe
diolan12 <dio_lantief21@outlook.com>
2021-11-24 13:03:59 +0700

    Update RestUpdateController.php

--- app/Http/Controllers/RestUpdateController.php
+++ app/Http/Controllers/RestUpdateController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\RoadRunnerFileHelper;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
 
@@ -27,5 +28,19 @@ class RestUpdateController extends RestController
             $this->json = $this->model->where('id', $id)->first();
         }
         return $this->response();
+    }
+    public function uploadAtColumn(Request $request, $table, $id, $column) {
+        if (!$request->hasFile("image")) {
+            return $this->error('Blank image file', 422);
+        }
+
+        $filename = Carbon::now()->format('h-i-s');
+        // $image = $request->file('image')->getPathName(); // file path
+        // $target = storage_path('app/assets/uploads/')."$filename.jpg";
+
+        $i = RoadRunnerFileHelper::parse($request->file('image'));
+        $isSuccess = $i->move(storage_path("app/assets/uploads/$table"), "$filename.jpg");
+
+        return $this->success(url("/assets/uploads/$table/" . $filename.".jpg"));
     }
 }
