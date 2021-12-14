200ced63833a2cd44353251d3178a2e860eac111
diolan12 <dio_lantief21@outlook.com>
2021-12-14 14:46:38 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -32,7 +32,6 @@ $router->group(['prefix' => 'api/{table}', 'middleware' => 'auth'], function () 
     // $router->post('/w/{column}/{value}', ['uses' => 'RestController@insertWhere']);
 
     $router->put('/{id}', ['uses' => 'RestUpdateController@update']);
-
     $router->post('/{id}/upload/{column}', ['uses' => 'RestUpdateController@uploadAtColumn']);
 
     $router->delete('/{id}', ['uses' => 'RestDeleteController@delete']);
