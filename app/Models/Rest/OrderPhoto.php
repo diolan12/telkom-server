5028f513f669b36a771eda33f079fd625eba92bf
diolan12 <dio_lantief21@outlook.com>
2021-12-14 14:47:08 +0700

    Create OrderPhoto.php

---
+++ app/Models/Rest/OrderPhoto.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models\Rest;
+
+use App\Models\BaseModel;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class OrderPhoto extends BaseModel
+{
+    use SoftDeletes;
+
+    public function validation()
+    {
+        return [
+            'order' => 'required',
+            'file' => '',
+            'description' => 'required'
+        ];
+    }
+    public function filter($data)
+    {
+        unset($data['id']);
+        return $data;
+    }
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'order', 'file', 'description'
+    ];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [];
+
+    public function getFileAttribute($value)
+    {
+        if ($value != null) {
+            return url('/assets/uploads/order/' . $value);
+        }
+        return $value;
+    }
+}
